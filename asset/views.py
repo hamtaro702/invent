a1cb1865cbf6f8887ae25c747b99488f085c0990
hamtaro702 <77424586+hamtaro702@users.noreply.github.com>
2021-01-14 10:33:03 +0700

    ีupdate view

--- asset/views.py
+++ asset/views.py
@@ -144,7 +144,6 @@ def readExcel(request):
                 print("i:" + str(i))
                 for cell in row:
                     print("j:" + str(j))
-                    
                     row_data.append(str(cell.value))
                     if j == 0:
                         assetlist.append(str(cell.value))
